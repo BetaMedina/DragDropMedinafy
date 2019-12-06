f5731febc4496e1afc5d116a27091549ee05a9ee
BetaMedina <joaopedromedinadeandrade@gmail.com>
2019-12-05 21:21:02 -0300

    DragandDrop

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from './styles';
+
+export default function Index() {
+  return (
+    <Container>
+      <h1>Medinafy</h1>
+    </Container>
+  );
+}
